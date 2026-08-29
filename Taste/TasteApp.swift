369a7b6db6bc6991283e56e2a5482ce5b5891234
kildishevaaa <aakildisheva@sibintek.ru>
2026-08-30 04:09:31 +0500

    Initial Commit

---
+++ Taste/TasteApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasteApp.swift
+//  Taste
+//
+//  Created by Alesya on 30.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct TasteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TasteTests/TasteTests.swift
@@ -0,0 +1,18 @@
+//
+//  TasteTests.swift
+//  TasteTests
+//
+//  Created by Alesya on 30.08.2026.
+//
+
+import Testing
+
+struct TasteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
